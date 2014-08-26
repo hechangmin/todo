3afe37a7b03cfbe41aef6a26373190df5d023526
hechangmin <hechangmin@gmail.com>
2014-08-26 11:44:02 +0800

    push all code

---
+++ README.md
@@ -0,0 +1,75 @@
+#todo.js#
+
+JavaScript asynchronous management.
+
+
+##usage##
+
+```js
+function task1(next){
+    setTimeout(function(){
+        console.log('task1', +new Date());
+        next(new Error('test error'));
+    }, 1000);
+}
+
+function task2(next){
+    setTimeout(function(){
+        console.log('task2', +new Date());
+        next(true);
+    }, 1000);
+}
+
+function taskA(next, params){
+    console.log('taskA', params, +new Date());
+    next('hello');
+}
+
+function taskB(next, params){
+    console.log('taskB', params, +new Date());
+    next();
+}
+
+function task3(next, params){
+    console.log('task3', params, +new Date());
+    next();
+}
+
+function task4(next,params){
+    console.log('task4',params,+new Date());
+    next();
+}
+
+function task5(next,params){
+    console.log('task5',params,+new Date());
+    next();
+}
+
+function task6(next,params){
+    console.log('task6',params,+new Date());
+    
+    setTimeout(function(){
+        for(var i = 0; i < 1000; i++){
+            next(i, 'notify');  
+        }
+    }, 1000);
+}
+
+function taskC(params){
+    console.log('...', params);
+}
+
+//ToDo([task2, task3]).done(taskA).done(task4);
+//ToDo([task1,task2]).done(taskA).done(task4);
+//ToDo([task1,task2]).done(taskA).always(task4);
+//ToDo([task1,task2]).fail(taskB).done(task4);
+//ToDo([task1,task2]).done(taskB).fail([task4,task5]);
+ToDo(task6).pregress(taskC);
+```
+-----------------
+
+##License##
+
+Released under the MIT license
+
+_*[hechangmin@gmail.com](mailto://hechangmin@gmail.com)*_
---
+++ todo.js
@@ -0,0 +1,179 @@
+/**
+ * 异步回调
+ *
+ * @author  hechangmin (hechangmin@gmail.com)
+ * @date    2014-08-21
+ */
+
+(function(undefined){
+
+	// 辅助方法，判断类型
+	var isType = function(type) {
+		return function(obj) {
+			return Object.prototype.toString.call(obj) === '[object ' + type + ']';
+		}
+	};
+	var isObject   = isType('Object');
+	var isError    = isType('Error');
+	var isArray    = Array.isArray || isType('Array');
+	var isFunction = isType('Function');
+
+	// 辅助方法，绑定默认参数
+	if(typeof Function.prototype.bind === 'undefined') {
+		Function.prototype.bind = function(thisArg) {
+			var f = this,
+				slice = Array.prototype.slice,
+				args = slice.call(arguments, 1);
+			return function() {
+				return f.apply(thisArg, args.concat(slice.call(arguments)));
+			};
+		};
+	}
+	
+	//定义几个常量，内部使用
+	var NOREADY = 0;
+	var READY = 1;
+	var START = 2;
+
+	var add = function(type, that, fns){
+		if(isArray(fns)){
+			that[type].push(fns.concat([]));
+		}else{
+			that[type].push(fns);
+		}
+
+		doTask(that, type);
+	};
+
+	var notify = function(that, params){
+		for(var j = 0, fn; fn = that['_progress'][j++];){
+			if(isFunction(fn)){
+				fn(params);
+			}else if(isArray(fn)){
+				for(var k = 0, step; step = fn[k++];){
+					if(isFunction(step)){
+						step(params);
+					}
+				}
+			}
+		}
+	};
+
+	//处理任务队列
+	var doTask = function(that, type){
+		var curTask;
+		var self = that;
+
+		if(READY !== that._status[type]){
+			return;
+		}
+
+		if(self[type].length > 0){
+			self._status[type] = START;
+			curTask = self[type].shift()
+
+			if(!isArray(curTask)){
+				curTask = [curTask]    	
+			}
+
+			self.nCurTimes = 0;
+			self.nTotalTimes = curTask.length;
+			
+			for (var i = 0, task; task = curTask[i++];) {
+				
+				if(isFunction(task)){
+					
+					var done = function(params, state){
+						
+						this.nCurTimes++;
+
+						if(arguments.length === 1){
+							this._params = params;
+						}
+						
+						if(this['_progress'].length > 0 && arguments.length > 1 && 'notify' === state){
+							notify(this, params);
+						}
+						
+						if(isError(params)){
+							this._status['_fail'] = READY;
+							doTask(this, '_fail');
+							this._status['_done'] = NOREADY;
+						}
+
+						if(this.nCurTimes >= this.nTotalTimes){
+							if(this._status[type] !== NOREADY){
+								this._status[type] = READY;
+								doTask(this, type);	
+							}
+						}
+					};
+
+					task(done.bind(self), self._params);
+				}
+			}
+		}
+	};
+
+	ToDo = function(fns){
+		return new ToDo.fn._init(fns);
+	};
+
+	ToDo.fn = ToDo.prototype;
+
+	ToDo.fn._init = function(fns){
+		this['_fail'] = [];
+		this['_done'] = [];
+		this['_progress'] = [];
+
+		this._status = {
+			_done : READY,
+			_fail : NOREADY,
+			_progress : NOREADY			
+		};
+
+		add('_done', this, fns);
+
+		return this;
+	};
+	
+	ToDo.fn.pregress = function(fnProgress) {
+		add('_progress', this, fnProgress);
+		return this;
+	};
+
+	ToDo.fn.then = function(fnDone, fnFail, fnProgress) {
+		if(fnDone){
+			add('_done', this, fnDone);	
+		}
+
+		if(fnFail){
+			add('_fail', this, fnFail);	
+		}
+		
+		if(fnProgress){
+			add('_progress', this, fnProgress);	
+		}
+		
+		return this;
+	};
+
+	ToDo.fn.always = function(fns){
+		add('_fail', this, fns);
+		add('_done', this, fns);
+		return this;
+	};
+
+	ToDo.fn.fail = function(fns){
+		add('_fail', this, fns);
+		return this;
+	};
+
+	ToDo.fn.done = function(fns){
+		add('_done', this, fns);
+		return this;
+	};
+
+	ToDo.fn._init.prototype = ToDo.fn;
+
+}());
